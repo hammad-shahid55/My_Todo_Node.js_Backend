6dc4d70f588a43f4fa3c4db0f8f8fb3c476e5fe6
hammad <shahidhammad179@gmail.com>
2026-01-15 21:02:30 +0500

    Add CORS middleware to Express server and update dependencies in package.json and bun.lock

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { connection, collectionName } from "./dbconfig.js";
+import cors from "cors";
 const app = express();
 
 
 app.use(express.json());
-
+app.use(cors());
 
 
 app.post("/add-task", async (req, res) => {
