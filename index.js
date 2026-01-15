d949881443a6c653d5853e3b5634f42771c3284d
hammad <shahidhammad179@gmail.com>
2026-01-15 21:14:25 +0500

    Refactor response formatting in task management endpoints and add a new endpoint to fetch tasks

--- index.js
+++ index.js
@@ -12,20 +12,40 @@ app.post("/add-task", async (req, res) => {
     const db = await connection();
     const collection = db.collection(collectionName);
     const result = await collection.insertOne(req.body);
-    if(result){
+    if (result) {
         res.send({
-            message:"Task added successfully",
-            success:true,
-            data:result
+            message: "Task added successfully",
+            success: true,
+            data: result
         });
-    }else{
+    } else {
         res.send({
-            message:"Task not added",
-            success:false,
-            data:result
+            message: "Task not added",
+            success: false,
+            data: result
         });
     }
-   
+
+})
+
+app.get("/tasks", async (req, res) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = await collection.find({}).toArray();
+    if (result) {
+        res.send({
+            message: "Tasks fetched successfully",
+            success: true,
+            data: result
+        });
+    } else {
+        res.send({
+            message: "Tasks not fetched",
+            success: false,
+            data: result
+        });
+    }
+
 })
 
 
@@ -33,8 +53,8 @@ app.get("/", (req, res) => {
 
     res.send(
         {
-            message:"basic api is working",
-            success:true
+            message: "basic api is working",
+            success: true
         }
     )
 })
